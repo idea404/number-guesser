3daccdd14316d9d533215ea166c1416273924afa
Dennis <10233439+idea404@users.noreply.github.com>
2023-03-20 09:37:17 +0100

    login flow created

--- client/src/App.vue
+++ client/src/App.vue
@@ -18,12 +18,13 @@
 </template>
 
 <script>
-// import * as zksync from "zksync-web3";
-import * as ethers from "ethers";
+import {} from "zksync-web3";
+import { ethers } from "ethers";
+import { Contract, Web3Provider, Provider } from "zksync-web3";
 // import LuckyGuessToken from "../../server/artifacts-zk/contracts/token.sol/LuckyGuessToken.json";
-import NumberGuessingGame from "../../server/artifacts-zk/contracts/game.sol/NumberGuessingGame.json";
+import NumberGuessingGameJson from "../../server/artifacts-zk/contracts/game.sol/NumberGuessingGame.json";
 
-// const NETWORK = "https://zksync2-testnet.zksync.dev";
+const NETWORK = "https://zksync2-testnet.zksync.dev";
 const GAME_CONTRACT_ADDRESS = "0x142210A453C17c7db82D84319f4dF25798ebB3F2";
 // const TOKEN_CONTRACT_ADDRESS = "0xc08aCd67c48818CaC2599Bfb585B411FFD5a0BF2";
 
@@ -47,17 +48,20 @@ export default {
   },
   methods: {
     async connectToZKSync() {
-      if (typeof window.ethereum !== "undefined") {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        this.provider = new ethers.providers.Web3Provider(window.ethereum);
-        this.signer = this.provider.getSigner();
-        // const zkSyncProvider = await zksync.Provider(NETWORK);
-        this.numberGuessingGame = new ethers.Contract(GAME_CONTRACT_ADDRESS, NumberGuessingGame.abi, this.provider);
-        this.rewardAvailable = ethers.utils.formatEther(await this.provider.getBalance(GAME_CONTRACT_ADDRESS));
-      } else {
-        console.log("Please install MetaMask!");
-      }
+        window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then(() => {
+          if (+window.ethereum.networkVersion == 280) {
+            this.provider = new Provider(NETWORK);
+            this.signer = new Web3Provider(window.ethereum).getSigner();
+            this.contract = new Contract(GAME_CONTRACT_ADDRESS, NumberGuessingGameJson.abi, this.signer);
+          } else {
+            console.log("Please switch to zkSync Testnet!");
+          }
+        })
+        .catch((e) => console.log(e));
     },
+
     async submitGuess() {
       this.isPlaying = true;
       const value = ethers.utils.parseEther("0.001");
@@ -65,19 +69,15 @@ export default {
         value: value,
       };
       try {
-        const tx = await this.numberGuessingGame.play(this.guess, overrides);
+        const tx = await this.contract.play(this.guess, overrides);
         const receipt = await tx.wait();
         const events = receipt.events || [];
         const lostEvent = events.find((event) => event.event === "Lost");
         if (lostEvent) {
           this.isLosing = true;
-          const numberGuessingGameBalance = ethers.utils.formatEther(await this.provider.getBalance(GAME_CONTRACT_ADDRESS));
-          this.rewardAvailable = numberGuessingGameBalance;
           return;
         }
         this.isWinning = true;
-        const numberGuessingGameBalance = ethers.utils.formatEther(await this.provider.getBalance(GAME_CONTRACT_ADDRESS));
-        this.rewardAvailable = numberGuessingGameBalance.sub(value);
       } catch (error) {
         console.log(error);
       }
